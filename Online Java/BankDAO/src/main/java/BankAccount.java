cd01f98631b49d08a7c6569104d94369a6d25536
Rajeeb Manandhar <rajeebmanandhar@Rajeebs-MacBook-Pro-2.local>
2021-07-22 12:10:04 -0700

    first commit

---
+++ Online Java/BankDAO/src/main/java/BankAccount.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class BankAccount {
+
+    private int accountnumber;
+    private float balance;
+    private String customerName;
+    private long phoneNumber;
+
+    BankAccount(String customerName, int accountnumber, float balance, long phonenumber, String Email){
+        this.accountnumber = accountnumber;
+        this.customerName = customerName;
+        this.balance = balance;
+        this.phoneNumber = phonenumber;
+        this.email = Email;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    public int getAccountnumber() {
+        return accountnumber;
+    }
+
+    public void setAccountnumber(int accountnumber) {
+        this.accountnumber = accountnumber;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
